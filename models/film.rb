33a5a3b8c02c6aec66753cb007093f6ab26b35a1
Joao <johnynecas@hotmail.com>
2017-10-13 10:54:59 +0100

    created film class

--- models/film.rb
+++ models/film.rb
@@ -3,10 +3,14 @@ require_relative("customer.rb")
 require_relative("../db/sql_runner.rb")
 class Film
 
-  attr_accessor()
+  attr_accessor(:title, :price)
+  attr_reader(:id)
 
-  def initialize()
-
+  
+  def initialize(film)
+    @id = film['id'].to_i if film['id']
+    @title = film['title']
+    @price = film['price'].to_i
   end
 
 
