4ecdd4a5a37790431ffa1314067c650be679d67c
Joao <johnynecas@hotmail.com>
2017-10-13 10:51:47 +0100

    created customer class

--- models/customer.rb
+++ models/customer.rb
@@ -1,10 +1,17 @@
 require("PG")
 require_relative("film.rb")
 require_relative("../db/sql_runner.rb")
+
+
 class Customer
 
-  attr_accessor()
-  def initialize()
+  attr_accessor(:name, :funds)
+  attr_reader(:id)
+  
+  def initialize(customer)
+    @id = customer['id'].to_i if customer['id']
+    @name = customer['name']
+    @funds = customer['name'].to_i
 
   end
 
