a621e151f3ceb93603c6da4089d4a80679b25a22
Joao <johnynecas@hotmail.com>
2017-10-13 15:52:09 +0100

    added crud methods for customer class

--- console.rb
+++ console.rb
@@ -5,11 +5,11 @@ require_relative("models/customer.rb")
 
 
 customer1 = Customer.new({'name'=>'Joao', 'funds'=> 100})
-
+customer1.save()
 customer2 = Customer.new({'name'=>'Jonathan', 'funds'=> 100})
-
+customer2.save()
 customer3 = Customer.new({'name'=> 'Margaret', 'funds'=> 50})
-
+customer3.save()
 
 film1 = Film.new({'title' => 'Blade Runner', 'price'=> 10})
 
